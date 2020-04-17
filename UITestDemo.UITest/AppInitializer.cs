4facd5f1f58e142f7c423b9a5a78361a225f1f7e
Shashank Singh <shashank.singh@successive.tech>
2020-04-17 12:40:04 +0530

    Add project files.

---
+++ UITestDemo.UITest/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestDemo.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("com.companyname.uitestdemo").StartApp();//ApkFile("C:/Users/shashanks/source/repos/UITestDemo/UITestDemo/UITestDemo.Android/bin/Release/com.companyname.uitestdemo.apk").
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ UITestDemo.UITest/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestDemo.UITest
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        [TestCase("email@email.com", "password", "LoggedIn successfully.")]
+        [TestCase("emai@email.com", "password", "Invalid email or password.")]
+        [TestCase("emai@email.com", "password", "LoggedIn successfully.")]
+        [TestCase("email@email.com", "password", "Invalid email or password.")]
+        public void PerformLogin(string email, string password, string result)
+        {
+            app.EnterText("Email", email);
+            app.EnterText("Password", password);
+
+            app.Tap("LoginButton");
+
+            var appResult = app.Query("Result").First(r => r.Text == result);
+            Assert.IsTrue(appResult != null, "Label not displayiong right result.");
+        }
+    }
+}
